91421b9845ae27be69b9ad258c0874d9e4f96581
huytocdo <ndhuyspkt@gmail.com>
2019-07-21 17:05:11 +0700

    add basic tour schema

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+// 1) CONNECT TO DATABASE
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
 
